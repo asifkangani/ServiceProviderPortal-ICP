5b13bcf0a977c78ee492d39ba164d6609d851b4c
lomate.ankitha <lomate.ankitha@iamdigitaltrust.com>
2026-02-18 16:35:38 +0530

    changes in trusted spoc and validations in create org

--- src/main/java/com/dtt/organization/repository/OrganisationCategoryRepo.java
+++ src/main/java/com/dtt/organization/repository/OrganisationCategoryRepo.java
@@ -2,7 +2,12 @@ package com.dtt.organization.repository;
 
 import com.dtt.organization.model.OrganisationCategories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Repository
